ed9f3c8d1303781dad85e42618c1f8d984fb1c63
Amina Kombo <komboamina@protonmail.com>
2023-10-18 01:03:48 +0300

    Implement getInitials()

--- index.php
+++ index.php
@@ -2,5 +2,38 @@
 
 defined('DS') || define('DS',DIRECTORY_SEPARATOR);
 
+include_once "src".DS."StringGenerator.php";
 
+include_once "src".DS."AvatarGenerator.php";
+
+if(isset($_GET['show'])){
+
+    //show generated image
+
+}
+else{
+
+    //show form
+
+    $avv=new AvatarGenerator();
+
+    $avv->generateAvatarFromName("Pedro Santos Domingas",250,250);
+    /*
+    $img = $avv->createDefaultAvatar(mt_rand(10,99)."");
+
+    ob_start();
+    imagepng($img);
+    $png=ob_get_clean();
+    $uri = "data:image/png;base64," . base64_encode($png);
+
+    echo "<img src='".$uri."' />";*/
+
+
+    /*
+    header("Content-Type: image/png");
+
+    imagepng($img);
+    imagedestroy($img);*/
+
+}
 
--- src/AvatarGenerator.php
+++ src/AvatarGenerator.php
@@ -6,6 +6,71 @@
 
 class AvatarGenerator{
 
+    public $randomizeBGColor=false;
+
+    public $randomizeDefaultInitials=true;
+
+    public $defaultInitials="Hi!";
+
+    public $activeFont='./src/OdudoMono-Bold.ttf';
+
+    private $minimumWidth=10;
+
+    private $minimumHeight=10;
+
+    private $maximumInitials=3;
+
+    public function __construct(){
+
+        
+
+    }
+
+    public function generateAvatarFromName($fullname,$width,$height){
+
+        $width=($width<$this->minimumWidth) ? $this->minimumWidth:$width;
+
+        $height=($height<$this->minimumHeight) ? $this->minimumHeight:$height;
+
+        $initials=$this->getInitials($fullname);
+
+    }
+
+    private function getInitials($fullname):string{
+
+        $initials="";
+
+        $names=explode(" ",$fullname);
+
+        $totalNames=count($names);
+
+        if(empty($names)){
+
+            $initials=($this->randomizeDefaultInitials) ?
+            \StringGenerator::generateRandomCode($this->maximumInitials):$this->defaultInitials;
+
+        }
+
+        else{
+
+            for($k=0;$k<$this->maximumInitials;$k++){
+
+                if(isset($names[$k])){
+
+                    $initials.=$names[$k][0];
+
+                }
+
+            }
+
+        }
+
+        return $initials;
+
+    }
+
+    /**unchanged from this tutorial:
+     * https://dev.to/thinkverse/creating-default-user-initial-avatars-in-php-7-1gf1 */
     function createDefaultAvatar(
         string $text = 'DEV',
         array $bgColor = [0, 0, 0],
@@ -13,7 +78,7 @@ class AvatarGenerator{
         int $fontSize = 140,
         int $width = 600,
         int $height = 600,
-        string $font = './arial.ttf'
+        string $font = './src/OdudoMono-Bold.ttf'
     ) {
         $image = @imagecreate($width, $height)
             or die("Cannot Initialize new GD image stream");
